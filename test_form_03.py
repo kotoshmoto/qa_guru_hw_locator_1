9247061d04e6602b234c6eacf31fa3bc602ae5cb
lyapin <lyapin@signatec.ru>
2026-07-23 19:25:04 +0700

    delete create driver in function

--- test_form_03.py
+++ test_form_03.py
@@ -56,112 +56,65 @@ def field_fills(driver, fullname, email, curr_addr, perm_addr):
     return result_box
 
 
-def test_fill_form_fields(form_factory):
-    # Запуск браузера Chrome
-    driver = webdriver.Chrome()
+def test_fill_form_fields():
+    result_box = field_fills(fullname="Петров Петр Петрович", email="test@example.com",
+                             curr_addr="г. Москва, Пушкина 101", perm_addr="г. Москва, Комсомольский проспект 42")
 
-    try:
-        result_box = form_factory(driver=driver, fullname="Петров Петр Петрович", email="test@example.com",
-                                  curr_addr="г. Москва, Пушкина 101", perm_addr="г. Москва, Комсомольский проспект 42")
-
-        # Проверяем, что в блоке результата появился введенный текст
-        assert "Петров Петр Петрович" in result_box.text
-        print("Тест успешно пройден!")
-
-    finally:
-        # Закрытие браузера в любом случае
-        driver.quit()
+    # Проверяем, что в блоке результата появился введенный текст
+    assert "Петров Петр Петрович" in result_box.text
+    print("Тест успешно пройден!")
 
 
 # Тесты валидные значения полей fullname, email, current_address, permanent_address
 @pytest.mark.parametrize("params", g_get_valid_fields())
-def test_fill_form_fields_with_valid_values(params):
-    # Запуск браузера Chrome
-    driver = webdriver.Chrome()
+def test_fill_form_fields_with_valid_values(params, driver):
     fullname, email, cur_addr, per_addr = params
 
-    try:
-        result_box = field_fills(driver=driver, fullname=fullname, email=email, curr_addr=cur_addr, perm_addr=per_addr)
+    result_box = field_fills(driver=driver, email=email, curr_addr=cur_addr, perm_addr=per_addr)
 
-        # Проверяем, что в блоке результата появился введенный текст
-        assert [fullname, email, cur_addr, per_addr] in result_box.text
-        print("Тест успешно пройден!")
-
-    finally:
-        # Закрытие браузера в любом случае
-        driver.quit()
+    # Проверяем, что в блоке результата появился введенный текст
+    assert [fullname, email, cur_addr, per_addr] in result_box.text
+    print("Тест успешно пройден!")
 
 
 # Тесты невалидное значение e-mail
 @pytest.mark.parametrize("email", g_get_invalid_email_and_injection_values())
-def test_fill_form_with_invalid_email(email):
-    # Запуск браузера Chrome
-    driver = webdriver.Chrome()
+def test_fill_form_with_invalid_email(email, driver):
+    field_fills(driver=driver, fullname="Захаров Геннадий Павлович", email=email,
+                curr_addr="г. Санкт-Петербург, Ленина 12, 22",
+                perm_addr="г. Санкт-Петербург, Ленина 12, 24")
 
-    try:
-        field_fills(driver=driver, fullname="Захаров Геннадий Павлович", email=email,
-                     curr_addr="г. Санкт-Петербург, Ленина 12, 22",
-                     perm_addr="г. Санкт-Петербург, Ленина 12, 24")
-
-        validation_message = driver.find_element(*EMAIL_INPUT).get_property("validationMessage")
-        assert (f'Адрес электронной почты должен содержать символ "@". В адресе "{email}"'
-                f' отсутствует символ "@".') in validation_message
-
-    finally:
-        # 5. Закрытие браузера в любом случае
-        driver.quit()
+    validation_message = driver.find_element(*EMAIL_INPUT).get_property("validationMessage")
+    assert (f'Адрес электронной почты должен содержать символ "@". В адресе "{email}"'
+            f' отсутствует символ "@".') in validation_message
 
 
 # Тесты невалидное значение fullname
 @pytest.mark.parametrize("fullname", g_get_invalid_fullname_and_injection_values())
-def test_fill_form_with_invalid_fullname(fullname):
-    # Запуск браузера Chrome
-    driver = webdriver.Chrome()
+def test_fill_form_with_invalid_fullname(fullname, driver):
+    field_fills(driver=driver, fullname=fullname, email="example@example.com",
+                curr_addr="г. Санкт-Петербург, Ленина 12, 22",
+                perm_addr="г. Санкт-Петербург, Ленина 12, 24")
 
-    try:
-        field_fills(driver=driver, fullname=fullname, email="example@example.com",
-                     curr_addr="г. Санкт-Петербург, Ленина 12, 22",
-                     perm_addr="г. Санкт-Петербург, Ленина 12, 24")
-
-        validation_message = driver.find_element(*FULLNAME_INPUT).get_property("validationMessage")
-        assert "Поле Full Name должно содержать символы латиницы или кириллицы" in validation_message
-
-    finally:
-        # Закрытие браузера в любом случае
-        driver.quit()
+    validation_message = driver.find_element(*FULLNAME_INPUT).get_property("validationMessage")
+    assert "Поле Full Name должно содержать символы латиницы или кириллицы" in validation_message
 
 
 # Тесты невалидное значение current address
 @pytest.mark.parametrize("curr_addr", g_get_invalid_curr_addr_and_injection_values())
-def test_fill_form_with_invalid_curr_addr(curr_addr):
-    # Запуск браузера Chrome
-    driver = webdriver.Chrome()
+def test_fill_form_with_invalid_curr_addr(curr_addr, driver):
+    field_fills(driver=driver, fullname="Иванов Василий Павлович", email="ivanov@example.ru",
+                curr_addr=curr_addr, perm_addr="г. Санкт-Петербург, Ленина 12, 24")
 
-    try:
-        field_fills(driver=driver, fullname="Иванов Василий Павлович", email="ivanov@example.ru",
-                     curr_addr=curr_addr, perm_addr="г. Санкт-Петербург, Ленина 12, 24")
-
-        validation_message = driver.find_element(*CURRENT_ADDRESS_INPUT).get_property("validationMessage")
-        assert "Поле Current Address должно содержать символы латиницы, кириллицы и цифры" in validation_message
-
-    finally:
-        # Закрытие браузера в любом случае
-        driver.quit()
+    validation_message = driver.find_element(*CURRENT_ADDRESS_INPUT).get_property("validationMessage")
+    assert "Поле Current Address должно содержать символы латиницы, кириллицы и цифры" in validation_message
 
 
 # # Тесты невалидное значение permanent address
 @pytest.mark.parametrize("perm_addr", g_get_invalid_perm_addr_and_injection_values())
-def test_fill_form_with_invalid_perm_addr(perm_addr):
-    # Запуск браузера Chrome
-    driver = webdriver.Chrome()
+def test_fill_form_with_invalid_perm_addr(perm_addr, driver):
+    field_fills(driver=driver, fullname="Иванов Василий Павлович", email="ivanov@example.ru",
+                curr_addr="г. Санкт-Петербург, Ленина 12, 24", perm_addr=perm_addr)
 
-    try:
-        field_fills(driver=driver, fullname="Иванов Василий Павлович", email="ivanov@example.ru",
-                     curr_addr="г. Санкт-Петербург, Ленина 12, 24", perm_addr=perm_addr)
-
-        validation_message = driver.find_element(*PERMANENT_ADDRESS_INPUT).get_property("validationMessage")
-        assert "Поле Permanent Address должно содержать символы латиницы, кириллицы и цифры" in validation_message
-
-    finally:
-        # Закрытие браузера в любом случае
-        driver.quit()
+    validation_message = driver.find_element(*PERMANENT_ADDRESS_INPUT).get_property("validationMessage")
+    assert "Поле Permanent Address должно содержать символы латиницы, кириллицы и цифры" in validation_message
